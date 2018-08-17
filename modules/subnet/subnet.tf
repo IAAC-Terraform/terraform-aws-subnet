86b44efa9f5aeaf59c7c1d146c2862411f12491e
Raghavan R <raghavan.r@capgemini.com>
2018-08-17 16:33:02 +1000

    fixing subnet_cidr variable type error

--- modules/subnet/subnet.tf
+++ modules/subnet/subnet.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "subnet" {
   count      = "${var.create_vpc ? var.count : 0}"
   vpc_id     = "${var.vpc_id}"
-  availability_zone = "${element(var.availability_zone, count.index}"
-  cidr_block = "${var.cidr_block[count.index]}"
+  availability_zone = "${element(var.availability_zone, count.index)}"
+  cidr_block = "${element(var.cidr_block, count.index)}"
 
   tags {
     Name = "${var.env}${var.name}-${count.index + 1}"
