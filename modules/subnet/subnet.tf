6a32b96f7109a986fd64668a33d25a718476a597
Raghavan R <raghavan.r@capgemini.com>
2018-08-17 16:29:25 +1000

    fixing variable type error

--- modules/subnet/subnet.tf
+++ modules/subnet/subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "subnet" {
   count      = "${var.create_vpc ? var.count : 0}"
   vpc_id     = "${var.vpc_id}"
-  availability_zone = "${var.availability_zone[count.index]}"
+  availability_zone = "${element(var.availability_zone, count.index}"
   cidr_block = "${var.cidr_block[count.index]}"
 
   tags {
