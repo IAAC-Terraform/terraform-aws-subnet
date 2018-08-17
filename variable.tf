c89b020241801de3b5e8880df0c8a2f7799c1bb5
Raghavan R <raghavan.r@capgemini.com>
2018-08-17 17:00:28 +1000

    changing var type

--- variable.tf
+++ variable.tf
@@ -2,9 +2,13 @@ variable "subnet_name" {}
 
 variable "vpc_id" {}
 
-variable "subnet_cidr_block" {}
+variable "subnet_cidr_block" {
+    type = "list"
+}
 
-variable "availability_zone" {}
+variable "availability_zone" {
+    type = "list"
+}
 
 variable "create_vpc" {}
 
