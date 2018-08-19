b1222699774feb1d63734f349f076a293f98b1f5
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 09:11:43 +1000

    sample

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "subnet_name" {}
 variable "vpc_id" {}
 
 variable "subnet_cidr_block" {
-    type = "list"
+    default = ["${split(",", var.subnet_cidr_block)}"]
 }
 
 variable "availability_zone" {
