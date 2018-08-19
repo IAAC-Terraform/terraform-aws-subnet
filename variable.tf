bc2d2842bfcfaac43818bea71b8e8e79e1ddb825
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 09:26:02 +1000

    bug fix

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "subnet_name" {}
 variable "vpc_id" {}
 
 variable "subnet_cidr_block" {
-    default = "${join(",", var.subnet_cidr_block)}"
+    default = ""
 }
 
 variable "availability_zone" {
