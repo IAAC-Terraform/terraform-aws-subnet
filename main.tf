4a2ad3375c6465f55bd7f95b4a0347107544c43a
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 10:09:51 +1000

    bug fix

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "subnet" {
   count      = "${var.create_vpc ? length(var.subnet_cidr_block) : 0}"
   vpc_id     = "${var.vpc_id}"
   availability_zone = "${element(var.availability_zone, count.index)}"
-  cidr_block = "${element(var.cidr_block, count.index)}"
+  cidr_block = "${element(var.subnet_cidr_block, count.index)}"
 
   tags {
     Name = "${var.env}${var.subnet_name}-${count.index + 1}"
